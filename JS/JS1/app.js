fed4dd09440fadc3b5337d906c34c62a70f5e35d
Gabriel Sahdo <gabriel.sahdo@icomp.ufam.edu.br>
2021-04-25 15:38:01 -0400

    Adição das atividades JS1

---
+++ JS/JS1/app.js
@@ -0,0 +1,14 @@
+
+for (let i = 1; i <= 10; i++) {
+    document.write("<table>");
+    document.write("<tr>");
+    document.write(`<th>Produtos de ${i}</th>`);
+    document.write("</tr>");
+    for (let j = 1; j <= 10; j++) {
+        document.write("<tr>")
+        document.write(`<td>${i}x${j}</td>`);
+        document.write(`<td>${i * j}</td>`)
+        document.write("</tr>");
+    }
+    document.write("</table>");
+}
